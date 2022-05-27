f0f19bf9b4a8d8c3ae83379639ce1c14fe46856a
younjaeyeal <jyyoun1021@gmail.com>
2022-05-28 01:22:08 +0900

    예외처리

--- src/main/java/org/codej/board/common/ExceptionHandler.java
+++ src/main/java/org/codej/board/common/ExceptionHandler.java
@@ -4,11 +4,13 @@ import lombok.extern.log4j.Log4j2;
 import org.mybatis.logging.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 @Log4j2
+@RequestMapping("/error")
 public class ExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
